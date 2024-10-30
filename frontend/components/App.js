bf970eb846a836110360b1cb0542c1a8f98b1d27
MrHyde801 <ianhyde801@gmail.com>
2024-10-30 17:35:02 -0600

    think i might have finished the assignement, pushing to get code graded

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -8,11 +8,46 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+
+  const[data, getData] = useState([]);
+  
+  useEffect(() => {
+    console.log('component mounted')
+    async function fetchPeopleAndPlanets() {
+      try {
+        let peopleRes = await axios.get(urlPeople);
+        let peopleData = peopleRes.data
+
+        let planetRes = await axios.get(urlPlanets);
+        let planetData = planetRes.data
+
+        console.log('Successful Fetch of Information')
+
+        const combinedData = peopleData.map(people => {
+          const matchingPlanet = planetData.find(planet => planet.id == people.homeworld)
+          return {
+            ...people,
+            homeworld: matchingPlanet
+          }
+        })
+        getData(combinedData)
+
+
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    fetchPeopleAndPlanets()
+  }, []);
+
+  
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {
+        data.map(pers => <Character key={pers.id} id={pers.id} name={pers.name} homeworld={pers.homeworld}  />)
+      }
     </div>
   )
 }
@@ -21,3 +56,15 @@ export default App
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
 if (typeof module !== 'undefined' && module.exports) module.exports = App
+
+
+
+
+
+
+//components needed from the mock example
+//<div></div> class=character-card
+  //<h3></h3> class=character-name
+
+  // <p>Planet: <span class= characterPlanet> </span> </p>   <--- this appears after the card is clicked on
+
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+function Character({ id, name, homeworld }) { // ❗ Add the props
+  const [render, setRender] = useState(false)
+  // console.log(homeworld)
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const toggleClick = () => {
+    render
+    ? setRender(false) : setRender(true)
+  }
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className="character-card" onClick={toggleClick}>
+      <h3 className="character-name">{name}</h3>
+      { render
+        ? <p>Planet: <span className="character-planet">{homeworld.name}</span></p>
+        : ''
+      }
     </div>
   )
 }
 
 export default Character
+
+
+//components needed from the mock example
+//<div></div> class=character-card
+  //<h3></h3> class=character-name
+
+  // <p>Planet: <span class= characterPlanet> </span> </p>   <--- this appears after the card is clicked on
+
